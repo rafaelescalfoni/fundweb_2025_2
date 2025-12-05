535f1ae793af68c4721b6cc7612a551ad98b972d
Pedro <pedro@email.com>
2025-12-05 19:47:50 -0300

    funcionalidade de busca por nome pronta

--- 251205/js/app.js
+++ 251205/js/app.js
@@ -1,27 +1,76 @@
 
-const URL = "https://restcountries.com/v3.1/all?fields=name,area,currency,capital,region,subregion,lang,population"
 
 //parametro: ?fields=param => ?fields=name
 
-const btnOrdenarNome = document.querySelector("#btnOrdenarNome")
-const btnOrdenarPopulacao = document.querySelector("#btnOrdenarPopulacao")
-const btnOrdenarArea = document.querySelector("#btnOrdenarArea")
 
+/**
+ * Função que exibe a lista de paises recuperados na tela
+ * @param {*} lista - relação de países que satisfaz alguma condição
+ */
+const exibirPaises = lista => {
+    const listaPaises = document.querySelector("#listaPaises")
+
+    const cards = lista.reduce((acum, pais) => {
+        const card = `<div class="pais" onclick="detalhesPais('${pais.name.common}')">
+            <img src="${pais.flag.png}" alt="bandeira">
+            <h3>${pais.name.common}</h3>
+            <p><strong>Capital:</strong>${pais.capital}</p>
+            <p><strong>População:</strong>${pais.population}</p>
+            </div>`
+        return acum + card
+    },"")
+    listaPaises.innerHTML = cards
+}
+
+/**
+ * Função que retorna todos os paises disponibilizados pela API
+ * @returns array com  todos os paises
+ */
 const carregarPaises = async () => {
-    // 1. estabelecer uma conexao
-    // 2. recuperar a promise
-  //  console.log(URL)
-    const resposta = await fetch(URL)
+    const url = "https://restcountries.com/v3.1/all?fields=name,area,currency,capital,region,subregion,lang,population,flag"
+
+    // 1. estabelecer uma conexao e recuperar a promise
+    const resposta = await fetch(url)
 
     // 3. carregar os dados da promises
     const dados = await resposta.json()
 
-    console.log(dados)
-
+    return dados
 }
 
-carregarPaises()
+const paises = carregarPaises()
 
+
+/**
+ * Função que filtra países por nome
+ * @param {*} palavra - chave de busca
+ * @param {*} paises - lista completa de paises a ser filtrada
+ */
+const buscarPorNome = (palavra, paises) => {
+
+    const fragMinusculo = palavra.toLowerCase()
+
+    return paises.filter(pais => {
+        const nomePaisMinusculo = pais.name.common.toLowerCase()
+        return nomePaisMinusculo.includes(fragMinusculo) 
+    })
+}
+
+const iptCampoBusca = document.querySelector("#campoBusca")
+
+iptCampoBusca.addEventListener("keyup", async () => {
+    //recebe a palavra de busca
+    const palavra = iptCampoBusca.value
+    
+    //carregar os paises
+    const paises = await carregarPaises()
+
+    //chamar a função que filtra
+    const paisesFiltrados = buscarPorNome(palavra, paises)
+
+    //exibir os paises
+    exibirPaises(paisesFiltrados)
+})
 
 /*
 ordenarPorNome()
@@ -32,7 +81,13 @@ ordenarPorArea()
 */
 
 
+/*
+ * comandos de tela 
+ */
 
+const btnOrdenarNome = document.querySelector("#btnOrdenarNome")
+const btnOrdenarPopulacao = document.querySelector("#btnOrdenarPopulacao")
+const btnOrdenarArea = document.querySelector("#btnOrdenarArea")
 
 
 btnOrdenarNome.addEventListener("click", ()=> {
