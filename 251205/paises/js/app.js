a8e042d64073b956bf5e8794d9ffb670196e83c3
Pedro <pedro@email.com>
2025-12-05 22:01:23 -0300

    app de países completo

--- 251205/paises/js/app.js
+++ 251205/paises/js/app.js
@@ -9,15 +9,17 @@ const exibirPaises = lista => {
     const listaPaises = document.querySelector("#listaPaises")
 
     const cards = lista.reduce((acum, pais) => {
-        const card = `<div class="pais" onclick="detalhesPais('${pais.name.common}')">
-            <img src="${pais.flag.png}" alt="bandeira">
+        const card = `<div class="pais">
+            <h1>${pais.flag}</h1>
             <h3>${pais.name.common}</h3>
             <p><strong>Capital:</strong>${pais.capital}</p>
-            <p><strong>População:</strong>${pais.population}</p>
+            <p><strong>População:</strong>${pais.population.toLocaleString()}</p>
             </div>`
         return acum + card
     },"")
     listaPaises.innerHTML = cards
+
+    exibirEstatisticas()
 }
 
 /**
@@ -102,6 +104,7 @@ const btnOrdenarNome = document.querySelector("#btnOrdenarNome")
 const btnOrdenarPopulacao = document.querySelector("#btnOrdenarPopulacao")
 const btnOrdenarArea = document.querySelector("#btnOrdenarArea")
 const selectBuscaRegiao = document.querySelector("#filtroRegiao")
+const listaPaises = document.querySelector("#listaPaises")
 
 btnOrdenarNome.addEventListener("click", ordenarPorNome)
 
@@ -118,4 +121,63 @@ selectBuscaRegiao.addEventListener("click", ()=>{
     paisesFiltrados = buscarPorRegiao(selectBuscaRegiao.value)
     //carregar os paises
     exibirPaises(paisesFiltrados)
+})
+
+const recuperaNomePais = (elemClicado) => {
+    const buscaH3 = divPai => {
+        for(elemFilho of divPai.children) {
+            if(elemFilho.tagName == "H3")
+                return elemFilho.textContent 
+        }
+    }
+    if(elemClicado.tagName == "DIV") return buscaH3(elemClicado)    
+    else if(elemClicado.tagName == "H3" 
+            || elemClicado.tagName == "IMG" 
+            || elemClicado.tagName == "P") return buscaH3(elemClicado.parentNode) // elemento pai do H3
+    
+    else  return buscaH3(elemClicado.parentNode.parentNode) // STRONG ESTÁ A DOIS NIVEIS DO DIV
+
+}
+
+const exibirEstatisticas = () => {
+    const areaTotal = paisesFiltrados.reduce((acum, pais) => acum+pais.area,0)
+    const populacaoTotal = paisesFiltrados.reduce((acum, pais) => acum+pais.population,0)
+    const totalPaisesExibidos = paisesFiltrados.length
+
+    const estatisticasHTML = document.querySelector("#estatisticas")
+
+    estatisticasHTML.innerHTML = `<h2>Estatísticas</h2>
+    <p><strong>Total de países exibidos: </strong>${totalPaisesExibidos}</p>
+    <p><strong>População total: </strong>${populacaoTotal.toLocaleString()}</p>
+    <p><strong>Área total: </strong>${areaTotal.toLocaleString()}</p>`
+
+}
+
+const exibirDetalhes = nomePais => {
+    const pais = buscarPorNome(nomePais)[0]
+    const popup = document.querySelector("#popup")
+    
+    popup.style.display= "block"
+    popup.classList.toggle("popupVisivel")
+
+    console.log(pais)
+    popup.innerHTML = `<div class="detalhes" >
+      <h1>${pais.flag}</h1>
+      <h3>${pais.name.common}</h3>
+      <p><strong>Capital:</strong>${pais.capital}</p>
+      <p><strong>População:</strong>${pais.populacao}</p>
+      <p><strong>Área:</strong>${pais.area}</p>
+      <p><strong>Região:</strong>${pais.region}</p>
+      <p><strong>Subregião:</strong>${pais.subregion}</p>
+    </div>`
+}
+
+listaPaises.addEventListener("click", (evento)=>{
+    const elemHTMLClicado = evento.target 
+    
+    // nome do pais está no h3
+    const nomePais = recuperaNomePais(elemHTMLClicado)
+    exibirDetalhes(nomePais)
+    //console.log(nomePais)
+
 })
